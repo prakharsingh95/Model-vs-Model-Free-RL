7782a1f065c41bd232c71181a5d26564390ae151
Prakhar Singh <prakharsingh95@gmail.com>
2019-11-24 00:27:50 -0600

    Change MDRNN training epochs to 30

--- settings.py
+++ settings.py
@@ -8,11 +8,9 @@ cuda = True
 device = torch.device('cuda')
 log_interval = 10
 num_epochs = 3
-num_epochs_mdrnn = 3
 seed = 2718
 train_test_split = 0.8
 
-mdrnn_batch_size = 16
 
 # Misc parameters
 action_space_size = 3
@@ -24,6 +22,9 @@ reduced_image_width = 64
 vae_latent_dim = 32
 
 # MDRNN Parameters
+num_epochs_mdrnn = 30
+mdrnn_batch_size = 16
+
 mdrnn_hidden_dim = 256
 mdrnn_num_gaussians = 5
 mdrnn_train_seq_len = 32
