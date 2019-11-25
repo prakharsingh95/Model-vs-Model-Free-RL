410b96740f39be63489031b87553eb553f499d4d
rjrock <ryanjrock95@gmail.com>
2019-11-25 02:08:18 -0600

    Add brownian policy

--- dataset/car_racing/rollout.py
+++ dataset/car_racing/rollout.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+'''Generate a rollout using a random policy. This is useful for getting data to
+trian the VAE.
+'''
 
 import argparse
 import cv2
@@ -6,7 +9,6 @@ import gym
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import scipy
 import json
 
 from PIL import Image
@@ -48,27 +50,56 @@ def parse_args():
     savedir = cwd/'rollout'
     parser.add_argument('--savedir', type=str, default=f'{savedir}',
                         help='Location to save rollouts')
+    parser.add_argument('--policy', type=str, default='random',
+                        help='random, brown')
     args = parser.parse_args()
     return args
 
 
-def rollout(env, savedir):
+def brownian_sample(action_space, seq_len, dt):
+    """ Sample a continuous policy.
+
+    Atm, action_space is supposed to be a box environment. The policy is
+    sampled as a brownian motion a_{t+1} = a_t + sqrt(dt) N(0, 1).
+
+    :args action_space: gym action space
+    :args seq_len: number of actions returned
+    :args dt: temporal discretization
+
+    :returns: sequence of seq_len actions
+    """
+    # Taken unmodified from ctallec implementation
+    actions = [action_space.sample()]
+    for _ in range(1, seq_len):
+        daction_dt = np.random.randn(*actions[-1].shape)
+        actions.append(
+            np.clip(actions[-1] + np.sqrt(dt) * daction_dt,
+                    action_space.low, action_space.high))
+    return actions
+
+
+def rollout(env, savedir, policy):
     images = Path('images')
-    random_val = np.random.randint(0, 2**31-1)
+    random_val = np.random.randint(2**31-1)
     max_frames = 1000
     env.reset()
     num_seen_frames = 0
     done = False
     metadata = []
 
+    if policy == 'random':
+        actions = [env.action_space.sample() for _ in range(max_frames+1)]
+    elif policy == 'brown':
+        actions = brownian_sample(env.action_space, max_frames+1, dt=1/50)
+
     state = None
     while not done:
         # TODO: Get action from RNN
-        action = env.action_space.sample()
+        action = actions[num_seen_frames]
         env.render('rgb_array')  # Look into why this call is necessary.
         next_state, reward, done, _ = env.step(action)
 
-        if num_seen_frames >= max_frames:
+        if num_seen_frames == max_frames:
             done = True
             reward = -100.0
 
@@ -81,7 +112,6 @@ def rollout(env, savedir):
             num_seen_frames += 1
         state = next_state
 
-
     with open(f'{savedir}/metadata.json', 'w') as f:
         content = json.dumps(metadata, indent=4)
         f.write(content)
@@ -93,7 +123,7 @@ def main():
     for i in range(args.num_rollouts):
         savedir = Path(args.savedir)/f'trajectory_{np.random.randint(2**31-1)}'
         savedir.mkdir(exist_ok=True, parents=True)
-        rollout(env, savedir)
+        rollout(env, savedir, policy=args.policy)
 
 
 if __name__ == '__main__':
