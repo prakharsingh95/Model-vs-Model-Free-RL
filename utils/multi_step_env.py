2dad2fc38a605660ef6ff28058a428ec84c75456
Prakhar Singh <prakharsingh95@gmail.com>
2019-11-29 20:55:22 -0600

    Add option to train with RGB frames

--- utils/multi_step_env.py
+++ utils/multi_step_env.py
@@ -14,9 +14,20 @@ from skimage import color, transform
 
 # from pympler import muppy, summary
 
+
 class MultiStepEnv(object):
 
-    def __init__(self, env_name: str, frame_stack_size=None, frame_skips=None, crop_height=None, crop_width=None, height=None, width=None, action_dict=None, max_neg_reward_steps=100):
+    def __init__(self, env_name: str, 
+        frame_stack_size=None, 
+        frame_skips=None, 
+        crop_height=None, 
+        crop_width=None, 
+        height=None, 
+        width=None,
+        enable_rgb=False,
+        action_dict=None, 
+        max_neg_reward_steps=100
+    ):
         assert height is not None and width is not None
         assert frame_stack_size is not None and frame_stack_size >= 2
         assert frame_skips is not None
@@ -27,6 +38,7 @@ class MultiStepEnv(object):
         self.crop_width = crop_width
         self.crop_height = crop_height
         self.max_neg_reward_steps = max_neg_reward_steps
+        self.enable_rgb = enable_rgb
 
         self.env = None
         self.buffer = None
@@ -35,7 +47,6 @@ class MultiStepEnv(object):
         self.height, self.width = height, width
         self.action_dict = action_dict
 
-
     def reset(self, video_log_file=None):
         if self.env is None:
             self.env = gym.make(self.env_name)
@@ -43,30 +54,25 @@ class MultiStepEnv(object):
         first_obs = self.process_frame(self.env.reset())
         # self.buffer = np.zeros((self.height,self.width, self.frame_stack_size)).astype(np.uint8)
         self.buffer = np.repeat(first_obs, self.frame_stack_size, axis=2)
-        
+
         if video_log_file is not None:
             env_height, env_width, _ = self.env.observation_space.shape
-            self.video_logger = VideoLogger(video_log_file, 
-                env_height, env_width) 
+            self.video_logger = VideoLogger(video_log_file,
+                                            env_height, env_width)
         else:
             self.video_logger = None
         self.neg_reward_steps = 0
 
-        return self.normalize(self.buffer).copy()
+        return self.buffer.copy()
 
     @property
     def num_actions(self):
         return len(self.action_dict)
 
-    @staticmethod
-    def normalize(obs):
-        # return (obs/255 * 2 - 1).astype(np.float32)
-        return obs
-
     @property
     def gym_env(self):
         return self.env
-    
+
     def process_frame(self, obs):
         # Crop
         if self.crop_height is not None:
@@ -76,23 +82,20 @@ class MultiStepEnv(object):
 
         # Resize
         if obs.shape[0] != self.height or obs.shape[1] != self.width:
-            obs = np.array(Image.fromarray(obs).resize((self.width, self.height), resample=Image.BICUBIC))
+            obs = np.array(Image.fromarray(obs).resize(
+                (self.width, self.height), resample=Image.BICUBIC))
 
-        # Convert to grayscale
-        obs = color.rgb2gray(obs)
-        # print(obs*2-1)
-        obs = (obs * 255).astype(np.uint8)
-        # print(obs/255.0 * 2 - 1)
-        # print(obs.max(), obs.min())
-        # raise "XYZ"
-        # obs = np.array(Image.fromarray(obs).convert('L'))
-
-        obs = np.expand_dims(obs, -1)
+        if not self.enable_rgb:
+            # Convert to grayscale
+            obs = color.rgb2gray(obs)
+            obs = (obs * 255).astype(np.uint8)
+            obs = np.expand_dims(obs, -1)
 
         return obs
 
     def random_action(self):
-        gas_actions = np.array([1 if a[1] == 1 and a[2] == 0 else 0 for a in self.action_dict.values()])
+        gas_actions = np.array(
+            [1 if a[1] == 1 and a[2] == 0 else 0 for a in self.action_dict.values()])
         weights = 14.0 * gas_actions + 1.0
         p = weights / np.sum(weights)
         # print(p)
@@ -103,7 +106,7 @@ class MultiStepEnv(object):
         total_reward = 0.0
         done = False
 
-        action = self.action_dict[action_idx] 
+        action = self.action_dict[action_idx]
 
         for _ in range(self.frame_skips):
             obs, reward, done, _ = self.env.step(action)
@@ -129,7 +132,11 @@ class MultiStepEnv(object):
 
         obs = self.process_frame(obs).copy()
 
-        self.buffer = np.concatenate((self.buffer[:,:,1:], obs), axis=-1).copy()
+        if not self.enable_rgb:
+            self.buffer = np.concatenate(
+            (self.buffer[:, :, 1:], obs), axis=-1).copy()
+        else:
+            self.buffer = np.concatenate(
+            (self.buffer[:, :, 3:], obs), axis=-1).copy()
 
-        return self.normalize(self.buffer).copy(), total_reward, done, {}
-
+        return self.buffer.copy(), total_reward, done, {}
